9c03cff17917625539c77b7c12fa580394017024
yuntaengtaeng <k76253218@gmail.com>
2023-05-29 15:26:42 +0900

    feat : storybook tailwind 적용
    - ui/Button stroybook 작성

---
+++ src/components/ui/Button.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from './Button';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SmallButton: Story = {
+  args: {
+    size: 'small',
+    children: 'Button',
+    className: '',
+    onClick: () => {},
+  },
+};
+
+export const MediumButton: Story = {
+  args: {
+    size: 'medium',
+    children: 'Button',
+    className: '',
+    onClick: () => {},
+  },
+};
+export const LargeButton: Story = {
+  args: {
+    size: 'large',
+    children: 'Button',
+    className: '',
+    onClick: () => {},
+  },
+};
