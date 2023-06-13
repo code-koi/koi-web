a8170d22b9996577004d1ba168de8f341f7d6a7b
yuntaengtaeng <k76253218@gmail.com>
2023-06-13 19:45:32 +0900

    feat : ui/cards/ResponseCodeReviewCard storybook 작성

---
+++ src/components/ui/cards/ResponseCodeReviewCard.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ResponseCodeReviewCard from './ResponseCodeReviewCard';
+import { withRouter } from 'storybook-addon-react-router-v6';
+
+const meta = {
+  title: 'ResponseCodeReviewCard',
+  component: ResponseCodeReviewCard,
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [withRouter],
+} satisfies Meta<typeof ResponseCodeReviewCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ResponseCodeReviewCardBase: Story = {
+  args: {
+    id: 'yuntaengtaeng',
+    link: '/test',
+    uid: 'testId1',
+    isKoiGuideVisible: true,
+    comment: '글',
+  },
+};
