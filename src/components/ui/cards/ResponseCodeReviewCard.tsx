5ca94d808a32cdc18fa238f3c1dcfd9345e78eeb
yuntaengtaeng <k76253218@gmail.com>
2023-06-13 19:45:11 +0900

    feat : ui/cards/ResponseCodeReviewCard 추가
    - 코드 리뷰 답을 보여주는 Card 컴포넌트
    - UI 구현

---
+++ src/components/ui/cards/ResponseCodeReviewCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Profile, { Props as ProfileProps } from '../Profile';
+import OutlineCard from '../OutlineCard';
+import Tooltip from '../ToolTip';
+
+/*
+    TODO : uid를 활용하여 onClick 구현 필요
+    - 🐟 클릭 시 Modal open 구현 필요
+*/
+
+interface Props extends ProfileProps {
+  comment: string;
+  uid: string;
+  isKoiGuideVisible: boolean;
+  containerClassName?: string;
+}
+
+const ResponseCodeReviewCard = ({
+  id,
+  profileIMG,
+  link,
+  comment,
+  isKoiGuideVisible,
+  containerClassName,
+}: Props) => {
+  return (
+    <div className={containerClassName}>
+      <div className="mb-5 flex items-center justify-between">
+        <Profile id={id} profileIMG={profileIMG} link={link} />
+
+        {isKoiGuideVisible && (
+          <Tooltip
+            labelElement={'🐟'}
+            tooltipElement={
+              <div className="w-max">코이를 지급 할 수 있어요</div>
+            }
+          />
+        )}
+      </div>
+      <OutlineCard
+        topElement={
+          <div className="mb-4 flex">
+            <p className="text-l whitespace-pre-line">{comment}</p>
+          </div>
+        }
+      />
+    </div>
+  );
+};
+
+export default ResponseCodeReviewCard;
