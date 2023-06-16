f98f2ffbda2cf6d47bd6bc654379ad77a83d6c41
yuntaengtaeng <k76253218@gmail.com>
2023-06-16 23:24:21 +0900

    feat : 공용 Types 추가
    - Activity, Skill, Profile Type 추가

---
+++ src/types/user.ts
@@ -0,0 +1,19 @@
+export interface Activity {
+  codeReview: number;
+}
+
+export interface Skill {
+  id: number;
+  name: string;
+}
+
+export interface Profile {
+  me: boolean;
+  nickname: string;
+  email: string;
+  years: number;
+  introduce?: string;
+  profileImageUrl?: string;
+  activity: Activity;
+  skills: Skill[];
+}
