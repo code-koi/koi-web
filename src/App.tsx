e3bdbc4d256006b25df4bb79ab728f430e6bc094
yuntaengtaeng <k76253218@gmail.com>
2023-06-22 00:05:38 +0900

    feat : App.tsx useDeviceType을 활용하여 환경에 따라 Footer를 랜더링 하도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import CodeReview from './pages/CodeReview';
 import Profile from './pages/Profile';
 import Footer from './components/ui/Footer';
+import MobileFooter from './components/ui/MobileFooter';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import useDeviceType from './hooks/useDeviceType';
@@ -29,7 +30,7 @@ function App() {
             </Routes>
           </section>
         </div>
-        <Footer />
+        {deviceType === 'Web' ? <Footer /> : <MobileFooter />}
       </BrowserRouter>
     </>
   );
