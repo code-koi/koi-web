8a94d8f947bbd732c15c4652a5afffe83df81c77
yuntaengtaeng <k76253218@gmail.com>
2023-06-22 00:02:27 +0900

    feat : ResponseCodeReviewTab Fiexd userId를 이용하여 API 연결

--- src/components/Profile/History/ResponseCodeReviewTab.tsx
+++ src/components/Profile/History/ResponseCodeReviewTab.tsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ResponseCodeReviewCard from '../../ui/cards/ResponseCodeReviewCard';
-
-const dummyData = [
-  {
-    id: 'yuntaengtaeng',
-    link: '/test',
-    uid: 'testId1',
-    comment: '글',
-  },
-  {
-    id: 'yuntaengtaeng',
-    link: '/test',
-    uid: 'testId2',
-    comment: '글',
-  },
-  {
-    id: 'yuntaengtaeng',
-    link: '/test',
-    uid: 'testId3',
-    comment: '글',
-  },
-];
+import { Comment } from '../../../types/review';
+import axios from 'axios';
 
 const ResponseCodeReviewTab = () => {
+  const [list, setList] = useState<Comment[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const {
+        data: { comments },
+      } = await axios.get<{ comments: Comment[] }>(
+        '/api/code-comments?userId=2'
+      );
+      setList(comments);
+    };
+
+    fetch();
+  }, []);
+
   return (
     <div className="mt-4 grid grid-cols-3 gap-4">
-      {dummyData.map((data) => (
-        <ResponseCodeReviewCard isKoiGuideVisible={false} {...data} />
+      {list.map((data) => (
+        <ResponseCodeReviewCard
+          {...data.user}
+          reviewId={data.reviewId}
+          comment={data.content}
+          isKoiGuideVisible={!!data.koiType}
+          koiGuideContent={
+            <>
+              <img
+                src={`/images/koi/${data.koiType}.jpg`}
+                alt="koi"
+                className="h-12 w-12"
+              />
+            </>
+          }
+        />
       ))}
     </div>
   );
