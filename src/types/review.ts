6cdd544be492ecbc85633922186a0823c5a5c941
yuntaengtaeng <k76253218@gmail.com>
2023-06-21 21:17:28 +0900

    feat : Type KoiType, Comment 추가 Review reviewId 추가

--- src/types/review.ts
+++ src/types/review.ts
@@ -1,9 +1,20 @@
 import { User } from './user';
 
+export type KoiType = 'FISHBOWL' | 'STREAM' | 'RIVER' | 'SEA';
+
 export interface Review {
+  reviewId: number;
   user: User;
   createdAt: string;
   title: string;
   status: 'PENDING' | 'RESOLVED';
   skills: string[];
+}
+
+export interface Comment {
+  user: User;
+  createdAt: string;
+  reviewId: number;
+  content: string;
+  koiType?: KoiType;
 }
