1ab4ace745539f6dbfd244a4e4bab492a39ce9e9
yuntaengtaeng <k76253218@gmail.com>
2023-06-21 21:22:28 +0900

    chore : App.tsx toastui-editor.css import 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import CodeReview from './pages/CodeReview';
 import Profile from './pages/Profile';
 import Footer from './components/ui/Footer';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 import useDeviceType from './hooks/useDeviceType';
 
