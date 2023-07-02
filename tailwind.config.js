f331ebbaaf6825874f36477781d034ed717da0ec
yuntaengtaeng <k76253218@gmail.com>
2023-07-02 15:48:09 +0900

    chore : 디자인 리뉴얼을 위해 tailwind custom Color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        B50: '#F8F8FF',
+        B100: '#F3F3FF',
+        B300: '#DFDEFD',
+        B500: '#9D9DB9',
+        B700: '#5F607A',
+        B900: '#1E2036',
+        PURPLE: '#8E48FF',
+        PINK: '#FF2897',
+        LIGHT_PINK: 'rgba(255, 40, 151, 0.10)',
+        GREEN: '#35CB00',
+        LIGHT_GREEN: 'rgba(113, 255, 64, 0.25)',
+        ORANGE: '#FFB341',
+        LIGHT_ORANGE: 'rgba(255, 179, 65, 0.10)',
+      },
+    },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 };
