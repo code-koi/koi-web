1c96ba06e2a46d0d006d60360d8aa32def488027
yuntaengtaeng <k76253218@gmail.com>
2023-07-02 18:48:04 +0900

    feat : ui/Tag Theme GRAY 추가 및 Props onClickHandler 추가

--- src/components/ui/Tag.tsx
+++ src/components/ui/Tag.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-type Theme = 'GREEN' | 'ORANGE' | 'PINK' | 'PURPLE';
+type Theme = 'GREEN' | 'ORANGE' | 'PINK' | 'PURPLE' | 'GRAY';
 
 interface Props {
   label: string;
   theme: Theme;
+  onClickHandler?: () => void;
 }
 
-const Tag = ({ label, theme }: Props) => {
+const Tag = ({ label, theme, onClickHandler }: Props) => {
   const [textColor, backgroundColor] = (() => {
     switch (theme) {
       case 'GREEN':
@@ -18,11 +19,14 @@ const Tag = ({ label, theme }: Props) => {
         return ['text-PINK', 'bg-LIGHT_PINK'];
       case 'PURPLE':
         return ['text-PURPLE', 'bg-B300'];
+      case 'GRAY':
+        return ['text-B900', 'bg-GRAY'];
     }
   })();
 
   return (
     <span
+      onClick={onClickHandler}
       className={`rounded px-3 py-1 text-sm ${textColor} ${backgroundColor}`}
     >
       {label}
