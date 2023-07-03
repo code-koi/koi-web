7491a3dd3bbca052a017f51d2b46a61c41d2da2d
yuntaengtaeng <k76253218@gmail.com>
2023-07-04 00:15:56 +0900

    feat : Profile(page) Web인 경우 Statistics, ActivityFeed 렌더링 추가

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import Information from '../components/Profile/Information';
 import History from '../components/Profile/History';
 import useDeviceType from '../hooks/useDeviceType';
+import ActivityFeed from '../components/Profile/ActivityFeed';
+import Statistics from '../components/Profile/Statistics';
 
 const Profile = () => {
   const type = useDeviceType();
   return (
     <div className="flex pt-7">
       <Information />
-      {type === 'Web' && <History />}
+      {type === 'Web' && (
+        <div className="mx-10 w-2/3">
+          <div className="mb-8 flex">
+            <Statistics />
+            <div className="mr-8"></div> {/* empty Space */}
+            <ActivityFeed />
+          </div>
+          <History />
+        </div>
+      )}
     </div>
   );
 };
