e405c0ae961913811f407e014bdabf676aad51a8
yuntaengtaeng <k76253218@gmail.com>
2023-07-03 19:19:11 +0900

    feat : Profile/History/RequestCodeReviewTab API 요청 시 Params id를 사용하도록 변경

--- src/components/Profile/History/RequestCodeReviewTab.tsx
+++ src/components/Profile/History/RequestCodeReviewTab.tsx
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react';
 import RequestCodeReviewCard from '../../ui/cards/RequestCodeReviewCard';
 import axios from 'axios';
 import { Review } from '../../../types/review';
+import { useParams } from 'react-router-dom';
 
 const RequestCodeReviewTab = () => {
+  const { id } = useParams();
   const [list, setList] = useState<Review[]>([]);
 
   useEffect(() => {
     const fetch = async () => {
       const {
         data: { reviews },
-      } = await axios.get<{ reviews: Review[] }>('/api/users/2/reviews');
+      } = await axios.get<{ reviews: Review[] }>(`/api/users/${id}/reviews`);
       setList(reviews);
     };
 
     fetch();
-  }, []);
+  }, [id]);
 
   return (
     <div className="mt-4 grid grid-cols-3 gap-4">
