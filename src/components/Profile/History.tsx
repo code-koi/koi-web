e005e13e6960455841fa565f9f4ec309e5dedd42
yuntaengtaeng <k76253218@gmail.com>
2023-07-04 00:15:09 +0900

    design : Profile/History TabSelector를 Chip으로 변경

--- src/components/Profile/History.tsx
+++ src/components/Profile/History.tsx
@@ -1,12 +1,19 @@
 import React, { useState, useCallback, useMemo } from 'react';
-import TabSelector from '../ui/TabSelector';
 import * as Components from './History//index';
+import Chip from '../ui/Chip';
+import Tag from '../ui/Tag';
+
+const TAB_LIST: readonly string[] = [
+  '요청한 코드 리뷰',
+  '남긴 코드 리뷰',
+  '즐겨찾기',
+];
 
 const History = () => {
   const [selectedTab, setSelectedTab] = useState('요청한 코드 리뷰');
 
-  const handleTabChange = useCallback((label: string) => {
-    setSelectedTab(label);
+  const handleTabChange = useCallback((id: string) => {
+    setSelectedTab(id);
   }, []);
 
   const renderTabContent = useMemo(() => {
@@ -23,16 +30,21 @@ const History = () => {
   }, [selectedTab]);
 
   return (
-    <div className="mx-10 w-2/3">
-      <TabSelector
-        containerClassName="w-full"
-        selectedLabel={selectedTab}
-        chipClassName="flex-auto"
-        labels={['요청한 코드 리뷰', '남긴 코드 리뷰', '즐겨찾기']}
-        onChangeHandler={handleTabChange}
-      />
+    <>
+      <div className="flex">
+        {TAB_LIST.map((text) => (
+          <Chip
+            key={text}
+            label={text}
+            id={text}
+            onClickHandler={handleTabChange}
+            margin="mr-2"
+            isActive={selectedTab === text}
+          />
+        ))}
+      </div>
       <div>{renderTabContent}</div>
-    </div>
+    </>
   );
 };
 
