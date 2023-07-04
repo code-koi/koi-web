f26da1e92c5fec4c20dfb8769039a80eceb81ef6
yuntaengtaeng <k76253218@gmail.com>
2023-07-05 00:21:16 +0900

    faet : dummy_data를 활용한 UI 요소들을 이용해 Home 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
-import Card from '../components/ui/Card';
-
-const dummyReview = [
-  { title: '제목1', sub: '#Spring #JAVA', nickname: 'sdcodebase' },
-  { title: '제목2', sub: '#Vue', nickname: 'yuntae' },
-];
-
-const dummyMentor = [
-  { title: '제목1', sub: '#Spring #JAVA', nickname: 'sdcodebase' },
-  { title: '제목2', sub: '#Vue', nickname: 'yuntae' },
-];
+import MainBanner from '../components/ui/banner/MainBanner';
+import PopularTags from '../components/Home/PopularTags';
+import PopularCodeReview from '../components/Home/PopularCodeReview';
 
 const Home = () => {
   return (
     <div className="flex flex-col">
-      <div className="my-20">
-        <h3 className="mb-5 font-bold">CodeKoi</h3>
-        <p className="font-semibold">코드 코이란 어쩌구 저쩌구</p>
+      <div className="mb-12 mt-9">
+        <MainBanner />
       </div>
-      <div className="my-20">
-        <h3 className="mb-5 font-bold">코드리뷰</h3>
-        <div className="flex gap-16">
-          {dummyReview.map((item) => (
-            <Card {...item} />
-          ))}
+      <div className="mx-16 flex">
+        <div className="flex-auto">
+          <PopularCodeReview />
         </div>
-      </div>
-      <div className="my-20">
-        <h3 className="mb-5 font-bold">멘토</h3>
-        <div className="flex gap-16">
-          {dummyMentor.map((item) => (
-            <Card {...item} />
-          ))}
+        <div className="pr-16"></div>
+        {/* empty space */}
+        <div className="w-2/12">
+          <PopularTags />
         </div>
       </div>
     </div>
