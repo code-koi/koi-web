ef67bf3cfc96691010763d9ea9d29556e92f2cd6
yuntaengtaeng <k76253218@gmail.com>
2023-07-05 21:55:56 +0900

    feat : header 디자인 리뉴얼로 nav의 Link 기능으로 인하여 storybook withRouter 추가

--- src/components/ui/Header.stories.ts
+++ src/components/ui/Header.stories.ts
@@ -1,12 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Header from './Header';
+import { withRouter } from 'storybook-addon-react-router-v6';
 
 const meta = {
   title: 'Header',
   component: Header,
   tags: ['autodocs'],
   argTypes: {},
+  decorators: [withRouter],
 } satisfies Meta<typeof Header>;
 
 export default meta;
