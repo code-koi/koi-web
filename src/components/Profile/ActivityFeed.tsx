5fb47db3f95450387eef1c793b311f19c75fbd5b
yuntaengtaeng <k76253218@gmail.com>
2023-07-09 03:11:11 +0900

    feat : 프로필 활동기록 dummy_data가 아닌 API response를 사용하도록 변경

--- src/components/Profile/ActivityFeed.tsx
+++ src/components/Profile/ActivityFeed.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const dummy_data = [
-  { text: 'Grouped version updates for Dependabot public beta', day: '2주 전' },
-  { text: 'TOTP codes for 2FA are now single-use', day: '4주 전' },
-  {
-    text: 'Security risk and coverage pages are now generally',
-    day: '한달 전',
-  },
-];
+interface Log {
+  createdAt: string;
+  log: string;
+  reviewId: number;
+}
 
 const ActivityFeed = () => {
+  const { id } = useParams();
+
+  const [logs, setLogs] = useState<Log[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const {
+        data: { logs },
+      } = await axios.get<{ logs: Log[] }>(`/api/users/${id}/logs`);
+      console.log(logs);
+      setLogs(logs);
+    };
+
+    fetch();
+  }, [id]);
+
   return (
     <div>
       <p className="mb-2 text-sm font-bold text-B900">활동 기록</p>
       <ul>
-        {dummy_data.map(({ text, day }, i) => (
+        {logs.map(({ reviewId, log, createdAt }, i) => (
           <li
-            key={text}
+            key={reviewId}
             className={`relative flex items-start pb-4 before:absolute before:bottom-0 before:left-[7.5px] before:top-0 before:bg-PURPLE before:content-[''] ${
-              dummy_data.length !== i + 1 && 'before:w-px'
+              logs.length !== i + 1 && 'before:w-px'
             }`}
           >
             <div className="h-4 w-4 rounded-full bg-PURPLE"></div>
             <div className="ml-4">
-              <p className="text-xs text-B500">{day}</p>
-              <p className="text-sm text-B700">{text}</p>
+              <p className="text-xs text-B500">{createdAt}</p>
+              <p className="text-sm text-B700">{log}</p>
             </div>
           </li>
         ))}
