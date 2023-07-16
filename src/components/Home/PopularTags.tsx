74d54fd7e9edb16ead96590344ff15577be1484d
yuntaengtaeng <k76253218@gmail.com>
2023-07-17 00:10:26 +0900

    feat : Home PopularTags(인기 태그) API 연동

--- src/components/Home/PopularTags.tsx
+++ src/components/Home/PopularTags.tsx
@@ -1,53 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chip from '../ui/Chip';
-
-const dummy_data = [
-  {
-    label: 'React',
-    id: 'React',
-  },
-  {
-    label: 'Vue',
-    id: 'Vue',
-  },
-  {
-    label: 'Java',
-    id: 'Java',
-  },
-  {
-    label: 'JavaScript',
-    id: 'JavaScript',
-  },
-
-  {
-    label: 'Spring',
-    id: 'Spring',
-  },
-  {
-    label: 'JWT',
-    id: 'JWT',
-  },
-  {
-    label: 'TypeScript',
-    id: 'TypeScript',
-  },
-  {
-    label: 'Cookie',
-    id: 'Cookie',
-  },
-  {
-    label: 'Express',
-    id: 'Express',
-  },
-];
+import axios from 'axios';
+import { Skill } from '../../types/user';
 
 const PopularTags = () => {
+  const [tags, setTags] = useState<Skill[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const {
+          data: { skills },
+        } = await axios.get<{ skills: Skill[] }>('/api/skills/rank');
+        setTags(skills);
+      } catch (error) {}
+    };
+
+    fetch();
+  }, []);
+
   return (
     <div className="rounded-3xl bg-white p-7 shadow-lg">
       <p className="mb-9 text-center text-xl font-semibold">인기태그</p>
       <div className="flex flex-wrap">
-        {dummy_data.map(({ label, id }) => (
-          <Chip label={label} id={id} isActive={true} margin="mr-2 mt-2" />
+        {tags.map(({ name, id }) => (
+          <Chip label={name} id={id} isActive={true} margin="mr-2 mt-2" />
         ))}
       </div>
     </div>
