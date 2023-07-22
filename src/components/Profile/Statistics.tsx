8c7c34898fb4cff6bd22b687af9a1191ba9fd00b
yuntaengtaeng <k76253218@gmail.com>
2023-07-22 16:43:50 +0900

    feat : Profile > Statistics API 연동
    - API 연동 및 API 스펙에 맞춰서 코드 변경

--- src/components/Profile/Statistics.tsx
+++ src/components/Profile/Statistics.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Skill } from '../../types/user';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
-const dummy_data: { [key: string]: number } = {
-  JAVA: 24,
-  Spring: 15,
-  React: 8,
-  Vue: 2,
-};
+interface SkillType extends Skill {
+  count: number;
+}
 
 const Statistics = () => {
+  const { id } = useParams();
   const refs = useRef<(HTMLSpanElement | null)[]>([]);
   const [minWidth, setMinWidth] = useState(0);
+  const [skillStatistics, setSkillStatistics] = useState<SkillType[]>([]);
 
-  const calcPercent = (key: string) => {
-    const total = Object.values(dummy_data).reduce((acc, cur) => acc + cur, 0);
-    return Math.round((dummy_data[key] / total) * 100);
+  const calcPercent = (count: number) => {
+    const total = skillStatistics.reduce((acc, cur) => acc + cur.count, 0);
+    return Math.round((count / total) * 100);
   };
 
   useEffect(() => {
@@ -24,6 +26,20 @@ const Statistics = () => {
     setMinWidth(max);
   }, [refs]);
 
+  useEffect(() => {
+    const fetch = async () => {
+      const {
+        data: { skills },
+      } = await axios.get<{ skills: SkillType[] }>(
+        `/api/users/${id}/skills/statistics`
+      );
+
+      setSkillStatistics(skills);
+    };
+
+    fetch();
+  }, [id]);
+
   return (
     <div className="flex-auto">
       <div className="rounded-3xl bg-B50 p-6">
@@ -31,44 +47,42 @@ const Statistics = () => {
           어떤 키워드를 많이 사용 했냐면요!
         </p>
         <ul>
-          {Object.entries(dummy_data).map(
-            ([k, v]: [k: string, value: number], index) => {
-              const width = calcPercent(k);
+          {skillStatistics.map(({ id, name, count }, index) => {
+            const width = calcPercent(count);
 
-              const [fontColor, barColor, countColor] = (() => {
-                switch (index) {
-                  case 0:
-                    return ['text-GREEN', 'bg-B600', 'text-B50'];
-                  case 1:
-                    return ['text-PINK', 'bg-B600', 'text-B50'];
-                  default:
-                    return ['text-B600', 'bg-B200', 'text-B700'];
-                }
-              })();
+            const [fontColor, barColor, countColor] = (() => {
+              switch (index) {
+                case 0:
+                  return ['text-GREEN', 'bg-B600', 'text-B50'];
+                case 1:
+                  return ['text-PINK', 'bg-B600', 'text-B50'];
+                default:
+                  return ['text-B600', 'bg-B200', 'text-B700'];
+              }
+            })();
 
-              return (
-                <li className="flex items-center pb-4" key={k}>
+            return (
+              <li className="flex items-center pb-4" key={id}>
+                <span
+                  ref={(itself) => (refs.current[index] = itself)}
+                  style={{ minWidth: `${minWidth}px` }}
+                  className={`${fontColor} mr-4 text-base font-semibold`}
+                >
+                  {name}
+                </span>
+                <div
+                  className={`h-10 ${barColor} relative flex items-center rounded-r-lg`}
+                  style={{ width: `${width}%` }}
+                >
                   <span
-                    ref={(itself) => (refs.current[index] = itself)}
-                    style={{ minWidth: `${minWidth}px` }}
-                    className={`${fontColor} mr-4 text-base font-semibold`}
+                    className={`${countColor} ml-auto mr-2 text-base font-medium`}
                   >
-                    {k}
+                    {count}
                   </span>
-                  <div
-                    className={`h-10 ${barColor} relative flex items-center rounded-r-lg`}
-                    style={{ width: `${width}%` }}
-                  >
-                    <span
-                      className={`${countColor} ml-auto mr-2 text-base font-medium`}
-                    >
-                      {v}
-                    </span>
-                  </div>
-                </li>
-              );
-            }
-          )}
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
