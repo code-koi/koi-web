33d164bbf11d778c611bf0915e5604a133a224c2
yuntaengtaeng <k76253218@gmail.com>
2023-07-22 17:17:59 +0900

    feat : Profile 키워드 분석 데이터가 없는 경우 안내 추가

--- src/components/Profile/Statistics.tsx
+++ src/components/Profile/Statistics.tsx
@@ -46,44 +46,59 @@ const Statistics = () => {
         <p className="mb-4 text-xl font-bold text-B900">
           어떤 키워드를 많이 사용 했냐면요!
         </p>
-        <ul>
-          {skillStatistics.map(({ id, name, count }, index) => {
-            const width = calcPercent(count);
+        {skillStatistics.length ? (
+          <ul>
+            {skillStatistics.map(({ id, name, count }, index) => {
+              const width = calcPercent(count);
 
-            const [fontColor, barColor, countColor] = (() => {
-              switch (index) {
-                case 0:
-                  return ['text-GREEN', 'bg-B600', 'text-B50'];
-                case 1:
-                  return ['text-PINK', 'bg-B600', 'text-B50'];
-                default:
-                  return ['text-B600', 'bg-B200', 'text-B700'];
-              }
-            })();
+              const [fontColor, barColor, countColor] = (() => {
+                switch (index) {
+                  case 0:
+                    return ['text-GREEN', 'bg-B600', 'text-B50'];
+                  case 1:
+                    return ['text-PINK', 'bg-B600', 'text-B50'];
+                  default:
+                    return ['text-B600', 'bg-B200', 'text-B700'];
+                }
+              })();
 
-            return (
-              <li className="flex items-center pb-4" key={id}>
-                <span
-                  ref={(itself) => (refs.current[index] = itself)}
-                  style={{ minWidth: `${minWidth}px` }}
-                  className={`${fontColor} mr-4 text-base font-semibold`}
-                >
-                  {name}
-                </span>
-                <div
-                  className={`h-10 ${barColor} relative flex items-center rounded-r-lg`}
-                  style={{ width: `${width}%` }}
-                >
+              return (
+                <li className="flex items-center pb-4" key={id}>
                   <span
-                    className={`${countColor} ml-auto mr-2 text-base font-medium`}
+                    ref={(itself) => (refs.current[index] = itself)}
+                    style={{ minWidth: `${minWidth}px` }}
+                    className={`${fontColor} mr-4 text-base font-semibold`}
                   >
-                    {count}
+                    {name}
                   </span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                  <div
+                    className={`h-10 ${barColor} relative flex items-center rounded-r-lg`}
+                    style={{ width: `${width}%` }}
+                  >
+                    <span
+                      className={`${countColor} ml-auto mr-2 text-base font-medium`}
+                    >
+                      {count}
+                    </span>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div className="flex flex-col items-center">
+            <p className="text-l mb-6 text-center font-semibold text-B600">
+              아직 키워드를 분석할 활동이 없어요
+              <br />
+              활동을 통해 내가 자주 활동한 키워드를 확인해보세요.
+            </p>
+            <img
+              src={`/images/worry_emoji.png`}
+              alt="worry_emoji"
+              className="w-32"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
