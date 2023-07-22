b0bbfa7f4bad75bafb17dcd60605b571e85f6d1d
yuntaengtaeng <k76253218@gmail.com>
2023-07-22 16:50:54 +0900

    feat : Home 인기있는 코드리뷰 API 연동

--- src/components/Home/PopularCodeReview/Request.tsx
+++ src/components/Home/PopularCodeReview/Request.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RequestCodeReviewCard from '../../ui/cards/RequestCodeReviewCard';
+import { Review } from '../../../types/review';
+import axios from 'axios';
 
 const reviews = [
   {
@@ -57,18 +59,32 @@ const reviews = [
 ];
 
 const Request = () => {
+  const [codeReviews, setCodeReviews] = useState<Review[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const {
+        data: { reviews },
+      } = await axios.get<{ reviews: Review[] }>('/api/code-reviews/hot');
+
+      setCodeReviews(reviews);
+    };
+
+    fetch();
+  }, []);
+
   return (
     <div>
       <p className="mb-6 text-2xl font-semibold text-B900">인기있는 코드리뷰</p>
       <div className="grid grid-cols-4 gap-x-6">
-        {reviews.map((data) => (
+        {codeReviews.map((data) => (
           <RequestCodeReviewCard
             key={data.reviewId}
             reviewId={data.reviewId}
             {...data.user}
             timestamp={data.createdAt}
             title={data.title}
-            isFavorite={data.isFavorite}
+            isFavorite={true}
             status={data.status}
             hashTag={data.skills}
           />
