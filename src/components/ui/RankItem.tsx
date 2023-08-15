91a794f44918839f5ab352559078fadbddf46812
yuntaengtaeng <k76253218@gmail.com>
2023-08-15 19:53:03 +0900

    design : RankItem Primary Color 변경 및 props rank 1 | 2 | 3 => number로 변경

--- src/components/ui/RankItem.tsx
+++ src/components/ui/RankItem.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 
 interface Props {
-  rank: 1 | 2 | 3;
+  rank: number;
   children: React.ReactNode;
-  margin?: string;
+  class?: string;
 }
-
-const RANK_COLOR = {
-  1: 'bg-blue-800',
-  2: 'bg-blue-500',
-  3: 'bg-blue-300',
-};
 
 const RankItem = (props: Props) => {
   return (
-    <div className="flex h-11">
-      <div
-        className={`${
-          RANK_COLOR[props.rank]
-        } flex w-11 items-center justify-center rounded-l-lg p-4 ${
-          props.margin || ''
-        }`}
-      >
+    <div className={`flex ${props.class}`}>
+      <div className="flex w-11 items-center justify-center rounded-l-lg bg-PURPLE p-4 text-white">
         {props.rank}
       </div>
-      <div className="flex items-center justify-center rounded-r-lg border-e px-4">
+      <div className="flex flex-auto items-center justify-start rounded-r-lg border-e px-4">
         {props.children}
       </div>
     </div>
