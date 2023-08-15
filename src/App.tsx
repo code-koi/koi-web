3eeae80d297690d2ac5963f4e60db527e370b4fd
yuntaengtaeng <k76253218@gmail.com>
2023-08-15 19:52:29 +0900

    add : CodeReviewDetai Page router

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import CodeReview from './pages/CodeReview';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
+import CodeReviewDetail from './pages/CodeReviewDetail';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 import useDeviceType from './hooks/useDeviceType';
@@ -23,6 +24,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/code-review" element={<CodeReview />} />
+              <Route path="/code-review/:id" element={<CodeReviewDetail />} />
               <Route path="/profile/:id" element={<Profile />} />
               <Route path="/login" element={<Login />} />
             </Routes>
