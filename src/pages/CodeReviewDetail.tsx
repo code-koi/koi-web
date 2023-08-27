e1d330d308eb666b867ac3c9da3a9041636534a2
yuntaengtaeng <k76253218@gmail.com>
2023-08-27 18:37:07 +0900

    feat : CodeReview Detail ArticleInformation, Comments 컴포넌트를 사용하도록 변경

--- src/pages/CodeReviewDetail.tsx
+++ src/pages/CodeReviewDetail.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import ArticleInformation from '../components/CodeReviewDetail/ArticleInformation';
+import Comments from '../components/CodeReviewDetail/Comments';
 
 const CodeReviewDetail = () => {
-  return <div>CodeReviewDetail</div>;
+  return (
+    <div className="flex h-full">
+      <div className="flex-auto p-12">
+        <ArticleInformation />
+      </div>
+      <div className="flex h-full w-1/2 flex-col bg-B50 p-12">
+        <Comments />
+      </div>
+    </div>
+  );
 };
 
 export default CodeReviewDetail;
