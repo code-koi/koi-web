773af78e5c0813270f80729beafefadad74f2ac0
yuntaengtaeng <k76253218@gmail.com>
2023-09-03 16:23:00 +0900

    feat : CodeReview API 연결

--- src/pages/CodeReview.tsx
+++ src/pages/CodeReview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Chip from '../components/ui/Chip';
 import RequestCodeReviewCard from '../components/ui/cards/RequestCodeReviewCard';
 import { Review } from '../types/review';
@@ -6,68 +6,19 @@ import RankItem from '../components/ui/RankItem';
 import Profile from '../components/ui/Profile';
 import FloatingWriteButton from '../components/ui/FloatingWriteButton';
 import TechSearch from '../components/ui/TechSearch';
-import { Skill } from '../types/user';
 import { VscSearch } from 'react-icons/vsc';
 import Search from '../components/CodeReview/Search';
 import axios from 'axios';
 import useTechSearch from '../hooks/useTechSearch';
+import useScrollYPosition from '../hooks/useScrollYPosition';
+import useDidUpdate from '../hooks/useDidUpdate';
 
-type Filter = '해결' | '미해결' | '전체';
+type Filter = 'PENDING' | 'RESOLVED' | 'ALL';
 
-const dummy_data: Review[] = [
-  {
-    id: 1,
-    title: '반갑습니다. 코드리뷰 남깁니다.',
-    status: 'PENDING',
-    createdAt: '2023-06-17T02:26:23',
-    user: {
-      profileImageUrl:
-        'https://code-koi.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2023-06-06+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+2.31.35.png',
-      nickname: '김선도1',
-      id: 2,
-    },
-    skills: ['Java', 'React'],
-  },
-  {
-    id: 2,
-    title: '이 코드 뭔가 이상한거같은데..?\n',
-    status: 'RESOLVED',
-    createdAt: '2023-06-17T02:28:41',
-    user: {
-      profileImageUrl:
-        'https://code-koi.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2023-06-06+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+2.31.35.png',
-      nickname: '김선도1',
-      id: 2,
-    },
-    skills: ['Java', 'React'],
-  },
-  {
-    id: 3,
-    title: 'ㄹ만이러미ㅏ;ㄴㅇ러ㅏㅣ;ㅁㄴㅇ',
-    status: 'RESOLVED',
-    createdAt: '2023-06-18T18:02:48',
-    user: {
-      profileImageUrl:
-        'https://code-koi.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2023-06-06+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+2.31.35.png',
-      nickname: '아저씨\n',
-      id: 3,
-    },
-    skills: ['Java', 'React'],
-  },
-  {
-    id: 4,
-    title: 'ㄹ만이러미ㅏ;ㄴㅇ러ㅏㅣ;ㅁㄴㅇ',
-    status: 'RESOLVED',
-    createdAt: '2023-06-18T18:02:48',
-    user: {
-      profileImageUrl:
-        'https://code-koi.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2023-06-06+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+2.31.35.png',
-      nickname: '아저씨\n',
-      id: 3,
-    },
-    skills: ['Java', 'React'],
-  },
-];
+type FilterOption = {
+  label: '해결' | '미해결' | '전체';
+  id: 'PENDING' | 'RESOLVED' | 'ALL';
+};
 
 const rank_dummy = [
   {
@@ -91,10 +42,28 @@ const rank_dummy = [
 ];
 
 const CodeReview = () => {
-  const [status, setStatus] = useState<Filter>('전체');
-  const filters: Filter[] = ['전체', '해결', '미해결'];
+  const [status, setStatus] = useState<Filter>('ALL');
+  const filters: FilterOption[] = [
+    {
+      label: '전체',
+      id: 'ALL',
+    },
+    {
+      label: '해결',
+      id: 'RESOLVED',
+    },
+    {
+      label: '미해결',
+      id: 'PENDING',
+    },
+  ];
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
+  const [isRequested, setIsRequested] = useState(false);
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const hasNext = useRef(false);
+  const lastId = useRef(0);
+  const { percentage } = useScrollYPosition();
 
   const {
     selectedTechs,
@@ -105,6 +74,76 @@ const CodeReview = () => {
   const openSearchScreen = () => {
     setIsSearchOpen(true);
   };
+
+  const getCodeReviews = async (shouldResetList: boolean) => {
+    if (isRequested) {
+      return;
+    }
+
+    setIsRequested(true);
+    try {
+      const query = Object.entries({
+        ...(!!searchQuery && {
+          title: searchQuery,
+        }),
+        ...(status !== 'ALL' && {
+          status,
+        }),
+        ...(!!selectedTechIds.length && {
+          skillIds: [...selectedTechIds],
+        }),
+        ...(!!lastId.current && {
+          lastId: lastId.current,
+        }),
+      })
+        .map(([k, v]) => {
+          switch (k) {
+            case 'skillIds':
+              return (v as number[])
+                .map((skillId) => `skillId=${skillId}`)
+                .join('&');
+            default:
+              return `${k}=${v}`;
+          }
+        })
+        .join('&');
+
+      const { data } = await axios.get<{
+        list: Review[];
+        hasNext: boolean;
+        lastId: number;
+      }>(`/api/code-reviews?${query}`);
+
+      console.log(data);
+
+      if (shouldResetList) {
+        setReviews(data.list);
+      } else {
+        setReviews([...reviews, ...data.list]);
+      }
+      lastId.current = data.lastId;
+      hasNext.current = data.hasNext;
+    } catch (error) {
+    } finally {
+      setIsRequested(false);
+    }
+  };
+
+  const initOptions = () => {
+    lastId.current = 0;
+    hasNext.current = false;
+  };
+
+  useDidUpdate(() => {
+    initOptions();
+    getCodeReviews(true);
+  }, [searchQuery, status, selectedTechs]);
+
+  useEffect(() => {
+    if (percentage >= 90 && hasNext.current) {
+      getCodeReviews(false);
+    }
+  }, [percentage]);
 
   return (
     <div className="mx-16">
@@ -134,7 +173,7 @@ const CodeReview = () => {
             onTechSelect={onTechSelect}
             onResetHandler={onResetHandler}
           />
-          <div className="flex flex-wrap gap-4">
+          <div className="flex flex-wrap gap-4 px-4">
             {selectedTechs.map(({ name, id }) => (
               <Chip key={name + id} id={id} label={name} isActive={true} />
             ))}
@@ -142,20 +181,20 @@ const CodeReview = () => {
         </div>
       </div>
       <div className="my-8">
-        {filters.map((label) => (
+        {filters.map((filter) => (
           <Chip
-            label={label}
-            id={label}
-            isActive={status === label}
+            label={filter.label}
+            id={filter.id}
+            isActive={status === filter.id}
             margin="mr-2"
             onClickHandler={() => {
-              setStatus(label);
+              setStatus(filter.id);
             }}
           />
         ))}
       </div>
-      <div className="grid grid-cols-4 gap-x-6">
-        {dummy_data.map((data) => (
+      <div className="grid grid-cols-4 gap-6">
+        {reviews.map((data) => (
           <RequestCodeReviewCard
             key={data.id + data.title}
             reviewId={data.id}
